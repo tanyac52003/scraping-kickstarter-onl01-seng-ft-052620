9bf9a56d1c68f94a366d127fc81fbc4033f305cb
Tanya Hodges <tanyac52003@gmail.com>
2020-06-20 14:30:24 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,3 +16,4 @@ end
 create_project_hash
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
+#location: project.css("")
