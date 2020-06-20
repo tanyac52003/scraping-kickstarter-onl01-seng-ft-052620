6c299b61510c8b0dc64657b5d7af6d709e8be4ea
Tanya Hodges <tanyac52003@gmail.com>
2020-06-20 14:26:01 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,8 +7,9 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   
   kickstarter = Nokogiri::HTML(html)
-#projects: kickstarter.css("li.project.grid_4")
+
   binding.pry 
 end
+#projects: kickstarter.css("li.project.grid_4")
 
 create_project_hash
